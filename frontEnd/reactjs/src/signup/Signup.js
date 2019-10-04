5feb7f5413ca8da216b1126b9cbea8158d8c7d9c
Fabien Dalla-valle <fabien.dalla-valle@mastertp>
2019-10-04 14:27:57 +0200

    Added signup component.

---
+++ frontEnd/reactjs/src/signup/Signup.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+    //class constructor whith given properties
+    constructor(props) {
+        super(props);        
+    }
+  
+     
+  //render function use to update the virtual dom
+  render() {
+
+    return (
+        <div className="container">
+    
+            <div className="col-md-6"> 
+                <label><b>Username</b></label> 
+                <input type="text" placeholder="Enter Username" name="uname" required/> 
+    
+                <label><b>Password</b></label> 
+                <input type="password" placeholder="Enter Password" name="psw" required/> 
+    
+                <label><b>Repeat password</b></label> 
+                <input type="password" placeholder="Repeat Password" name="rpsw" required/> 
+
+                <button type="submit">Sign up</button> 
+            </div> 
+    
+        </div>
+    );
+  }
+}
+
+export default Signup;
