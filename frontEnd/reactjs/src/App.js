93dda755a16bb88a0f3707a9e1537c51fb5bad0f
Fabien Dalla-valle <fabien.dalla-valle@mastertp>
2019-10-04 12:14:09 +0200

    Added login component.

--- frontEnd/reactjs/src/App.js
+++ frontEnd/reactjs/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './login/Login';
 
 function App() {
   return (
@@ -19,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Login />
     </div>
   );
 }
---
+++ frontEnd/reactjs/src/login/Login.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    //class constructor whith given properties
+    constructor(props) {
+        super(props);        
+    }
+  
+     
+  //render function use to update the virtual dom
+  render() {
+
+    return (
+            <div className="panel panel-default">
+                Login
+            </div>
+    );
+  }
+}
+
+export default Login;
