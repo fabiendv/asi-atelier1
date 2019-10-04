fbfd8fabd20c219794858725c8aa0a9bb3e0c7c8
Fabien Dalla-valle <fabien.dalla-valle@mastertp>
2019-10-04 14:17:25 +0200

    Updated page content.

--- frontEnd/reactjs/src/login/Login.js
+++ frontEnd/reactjs/src/login/Login.js
@@ -11,9 +11,26 @@ class Login extends Component {
   render() {
 
     return (
-            <div className="panel panel-default">
-                Login
-            </div>
+        <div className="container">
+    
+            <div className="col-md-6"> 
+                <label><b>Username</b></label> 
+                <input type="text" placeholder="Enter Username" name="uname" required/> 
+    
+                <label><b>Password</b></label> 
+                <input type="password" placeholder="Enter Password" name="psw" required/> 
+    
+                <button type="submit">Login</button> 
+            </div> 
+    
+            <div className="col-md-6" > 
+                <span className="psw">
+                    <a href="#">
+                        Create an account
+                    </a>
+                </span> 
+            </div> 
+        </div>
     );
   }
 }
