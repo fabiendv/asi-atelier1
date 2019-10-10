e8a9202f5280bac3666a681499a57459fd7885bf
Maxime Delahodde <maxime.delahodde@mastertp>
2019-10-10 14:15:14 +0200

    Corrected json object.

--- frontEnd/reactjs/src/card/containers/Order.js
+++ frontEnd/reactjs/src/card/containers/Order.js
@@ -10,36 +10,37 @@ class Order extends Component {
       orderType: this.props.orderType,
       selectedCard: 0
     };
-    this.cards ={ 
-          "0": {
-          "id": "0",
-          "name": "Superman",
-          "description": "desc superman",
-          "family": "DC",
-          "affinity": "",
-          "imgUrl": "https://static.hitek.fr/img/actualite/2017/06/27/i_deadpool-2.jpg",
-          "smallImgUrl": "https://vignette.wikia.nocookie.net/lego/images/4/48/76096_Minifigure_04.jpg/revision/latest/scale-to-width-down/250?cb=20190729133554",
-          "energy": "10",
-          "hp": "10",
-          "defence": "2",
-          "attack": "5",
-          "price": "1000"
-        },
-        "1": {
-          "id":"1",
-          "name": "Batman",
-          "description": "desc batman",
-          "family": "fam",
-          "affinity": "afini",
-          "imgUrl": "https://cdn3.iconfinder.com/data/icons/batman/154/batman-512.png",
-          "smallImgUrl": "https://static.fnac-static.com/multimedia/Images/8F/8F/7D/66/6716815-1505-1540-1/tsp20171122191008/Lego-lgtob12b-lego-batman-movie-lampe-torche-batman.jpg",
-          "energy": "1000",
-          "hp": "10",
-          "defence": "5",
-          "attack": "0",
-          "price": "100"
-        }
+    this.cards =[
+      {
+        "id": "0",
+        "name": "Superman",
+        "description": "desc superman",
+        "family": "DC",
+        "affinity": "",
+        "imgUrl": "https://static.hitek.fr/img/actualite/2017/06/27/i_deadpool-2.jpg",
+        "smallImgUrl": "https://vignette.wikia.nocookie.net/lego/images/4/48/76096_Minifigure_04.jpg/revision/latest/scale-to-width-down/250?cb=20190729133554",
+        "energy": "10",
+        "hp": "10",
+        "defence": "2",
+        "attack": "5",
+        "price": "1000"
+      },
+      {
+        "id":"1",
+        "name": "Batman",
+        "description": "desc batman",
+        "family": "fam",
+        "affinity": "afini",
+        "imgUrl": "https://cdn3.iconfinder.com/data/icons/batman/154/batman-512.png",
+        "smallImgUrl": "https://static.fnac-static.com/multimedia/Images/8F/8F/7D/66/6716815-1505-1540-1/tsp20171122191008/Lego-lgtob12b-lego-batman-movie-lampe-torche-batman.jpg",
+        "energy": "1000",
+        "hp": "10",
+        "defence": "5",
+        "attack": "0",
+        "price": "100"
       }
+    ]
+         
   };
 
   render() {
