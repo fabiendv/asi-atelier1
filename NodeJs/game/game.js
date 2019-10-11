adafc447f5761aff5b81471f76e7665cb46b90e9
Thibault Caussanel <thibault.caussanel@mastertp>
2019-10-11 09:55:29 +0200

    update game

--- NodeJs/game/game.js
+++ NodeJs/game/game.js
@@ -3,10 +3,13 @@ module.exports = class Game {
         this.id = id;
         this.first_player = first_player;
         this.second_player = second_player;
+        this.first_player_cards = null;
+        this.second_player_cards = null;
     }
 
     isReady() {
-        if (this.first_user != null && this.second_user != null){
+        if (this.first_user != null && this.second_user != null 
+            && this.first_player_cards != null && this.second_player_cards != null){
             return true;
         }
         else{
