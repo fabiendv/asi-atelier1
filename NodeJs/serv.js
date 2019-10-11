9c4f9cdf9a7bf404ec71de9e77c333946bd66539
Thibault Caussanel <thibault.caussanel@mastertp>
2019-10-11 08:47:54 +0200

    initialisation du dossier NodeJs

---
+++ NodeJs/serv.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const port = 8085;
+
+app.use(express.static('front'));
+
+app.get('/', (req, res) => {
+	res.send('Hello word')
+})
+
+app.get('*', (req, res) => {
+	res.send('Error 404')
+})
+
+app.listen(port, () => {
+	console.log('Serveur: localhost:', port)
+})
