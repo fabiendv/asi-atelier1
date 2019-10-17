61b6ca9b9d27cce03aa68efd6f294d768dc8670e
F-DALLA-VALLE <37407695+F-DALLA-VALLE@users.noreply.github.com>
2019-10-17 10:56:28 +0200

    Created chat inside reactjs. Need to test.

--- frontEnd/reactjs/src/chat/containers/chat.js
+++ frontEnd/reactjs/src/chat/containers/chat.js
@@ -8,11 +8,97 @@ class Chat extends Component{
 		};
 	}
 
+	componentDidMount(){
+        let that = this;
+        // that.setState({view:"wait"});
+        // let username = document.getElementById
+        let socket = require('socket.io-client')('http://localhost:1338');
+		socket.emit('login', {
+            
+			username : Math.trunc(Math.random()*100),
+			mail     : '@cpe.fr',  
+			usercolor:''  
+		});
+	
+		socket.on('newusr', function(user){
+		   $('#users').append('<div class="item" id="'+user.username+'" data-value="jd"><i class="jd user circle icon"></i>'+user.username+'</div>')
+		});
+	
+		socket.on('currentUser', function(user){
+			$('#current-user').append('<div class="column">'+user.username+'</div> ')
+		});
+	
+		socket.on('deleteUser', function(user){
+			document.getElementById(user.username).remove();
+		});
+	
+		// $( "button" ).click(function( event ) {
+		// 	var data={};
+		// 	data.message = $("textarea").val();
+		// 	data.username = $("#current-user .column:last-child").text();
+		// 	data.target = document.getElementById("users").getElementsByClassName("active selected")[0].id;
+		// 	socket.emit("messageSent",data);
+		// 	$("textarea").val('');
+			
+		// });
+	
+	
+		socket.on('newMessage',function(data){
+			if(data.username == $("#current-user .column:last-child").text()){
+				$('#messages').append('<div class="ui raised segment"><a class="ui ribbon label" style="background-color:'+data.color+'">'+data.username+'</a><span>'+data.hours+':'+data.minutes+'</span><p>'+data.message+'</p></div>')        
+			}else{
+				$('#messages').append('<div class="ui raised segment"><a class="ui right ribbon label" style="background-color:'+data.color+'">'+data.username+'</a><span>'+data.hours+':'+data.minutes+'</span><p>'+data.message+'</p></div>')
+			} 
+		});
+    }
+
 	render(){
 		return (
             <div className="chat">
-                <h3>The chat is here.</h3>
+                <div className="ui segment">
+                    <div className="ui five column grid">
+                        <div className="column">
+                            <div className="ui segment">
+                                <div className="ui top attached label">
+                                    <div className="ui two column grid">
+                                        <div className="column">
+											Chat
+										</div>
+										<div className="column">
+												<div className="ui two column grid" id="current-user">
+														<div className="column">
+															<i className="user circle icon"></i>
+														</div>
+												</div>
+										</div>
+                                    </div>
+                                </div>
+                        	</div>
+                            <div className="ui fluid search selection dropdown" id="usersConnected">
+                                    <input type="hidden" name="country"/>
+                                    <i className="dropdown icon"></i>
+                                    <div className="default text">
+										Select User
+									</div>
+                                    <div className="menu" id="users">
+                                    </div>
+                            </div>
+                            <div className="ui segment" id="messages">
+
+                            </div>
+                            <div className="ui form">
+                                <div className="field">
+                                    <textarea rows="2"></textarea>
+                                </div>
+                            </div>
+                            <button className="fluid ui right labeled icon button">
+                                <i className="right arrow icon"></i>
+                                Send
+                            </button>
+                        </div>
+                    </div>
             </div>
+        </div>
         );
 	}
 }
