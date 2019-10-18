57b443714b62861bdc92bca962b1e2a6b862550e
F-DALLA-VALLE <37407695+F-DALLA-VALLE@users.noreply.github.com>
2019-10-18 21:47:32 +0200

    Corrected bug.

--- frontEnd/reactjs/src/chat/containers/chat.js
+++ frontEnd/reactjs/src/chat/containers/chat.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import $ from 'jquery';
+import Select from 'react-select';
 
 const socket = require('socket.io-client')('http://localhost:1338');
 
@@ -8,65 +9,54 @@ class Chat extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
+			userConnectedList:[],
+			talkingTo:""
 		};
+		this.sendMessage = this.sendMessage.bind(this);
 	}
 
-	// componentWillMount(){
-	// 	let that = this;
-	// 	window.$ = $;
-    //     that.setState({view:"wait"});
-	// 	let username = document.getElementById;
-	// 	console.log("I AM IN WILLMOUNT");
-    //     let socket = require('socket.io-client')('http://localhost:1338');
-	// 	socket.emit('login', {
-	// 		username : Math.trunc(Math.random()*100),
-	// 		mail     : '@cpe.fr',  
-	// 		usercolor:''  
-	// 	});
-	
-	// 	socket.on('newusr', function(user){
-	// 		console.log("There is a new user: "+JSON.stringify(user));
-	// 	   $('#users').append('<div class="item" id="'+user.username+'" data-value="jd"><i class="jd user circle icon"></i>'+user.username+'</div>')
-	// 	});
-	
-	// 	socket.on('currentUser', function(user){
-	// 		console.log("I am in currentUser: "+JSON.stringify(user));
-	// 		$('#current-user').append('<div class="column">'+user.username+'</div> ')
-	// 	});
-	
-	// 	socket.on('deleteUser', function(user){
-	// 		document.getElementById(user.username).remove();
-	// 	});
-	
-	// 	$( "button" ).click(function( event ) {
-	// 		var data={};
-	// 		data.message = $("textarea").val();
-	// 		data.username = $("#current-user .column:last-child").text();
-	// 		data.target = document.getElementById("users").getElementsByClassName("active selected")[0].id;
-	// 		socket.emit("messageSent",data);
-	// 		$("textarea").val('');
-	// 	});
-	
-	
-	// 	socket.on('newMessage',function(data){
-	// 		if(data.username == $("#current-user .column:last-child").text()){
-	// 			$('#messages').append('<div class="ui raised segment"><a class="ui ribbon label" style="background-color:'+data.color+'">'+data.username+'</a><span>'+data.hours+':'+data.minutes+'</span><p>'+data.message+'</p></div>')        
-	// 		}else{
-	// 			$('#messages').append('<div class="ui raised segment"><a class="ui right ribbon label" style="background-color:'+data.color+'">'+data.username+'</a><span>'+data.hours+':'+data.minutes+'</span><p>'+data.message+'</p></div>')
-	// 		} 
-	// 	});
-    // }
-
-	render(){
+	sendMessage(){
+		var data={};
 		window.$ = $;
+		data.message = $("textarea").val();
+		data.username = $("#current-user .column:last-child").text();
+		// data.target = document.getElementById("users").getElementsByClassName("active selected")[0].id;
+		data.target = this.state.talkingTo;
+		socket.emit("messageSent",data);
+		$("textarea").val('');
+	}
+
+	componentDidMount(){
+		// TODO: envoyer les informations du prop
 		socket.emit('login', {
 			username : Math.trunc(Math.random()*100),
 			mail     : '@cpe.fr',  
 			usercolor:''  
 		});
+	}
+
+	handleChangeUser = selectedUser => {
+		this.setState(
+		  { selectedUser },
+		  () => {
+			  console.log(`Option selected:`, selectedUser);
+			  this.state.talkingTo = selectedUser.label;
+		  }
+		);
+	  };
+
+	render(){
+		var that = this;
+		console.log("Render");
+		console.log("This is the log: "+ JSON.stringify(this.state.userConnectedList));
 		socket.on('newusr', function(user){
+			// TODO : Do not add the current user to the list
+			// if(this.props.user.username!=user.username){
+				
+			// }
 			console.log("There is a new user: "+JSON.stringify(user));
-		   $('#users').append('<div class="item" id="'+user.username+'" data-value="jd"><i class="jd user circle icon"></i>'+user.username+'</div>')
+			that.state.userConnectedList.push({label:user.username,value:user.socketId});
+			// $('#users').append('<div class="dropdown-item" id="'+user.username+'" data-value="jd"}><i class="jd user circle icon"></i>'+user.username+'</div>')
 		});
 	
 		socket.on('currentUser', function(user){
@@ -76,18 +66,7 @@ class Chat extends Component{
 	
 		socket.on('deleteUser', function(user){
 			document.getElementById(user.username).remove();
-		});
-
-			
-		$( "button" ).click(function( event ) {
-			var data={};
-			data.message = $("textarea").val();
-			data.username = $("#current-user .column:last-child").text();
-			data.target = document.getElementById("users").getElementsByClassName("active selected")[0].id;
-			socket.emit("messageSent",data);
-			$("textarea").val('');
-		});
-	
+		});	
 	
 		socket.on('newMessage',function(data){
 			if(data.username == $("#current-user .column:last-child").text()){
@@ -96,11 +75,12 @@ class Chat extends Component{
 				$('#messages').append('<div class="ui raised segment"><a class="ui right ribbon label" style="background-color:'+data.color+'">'+data.username+'</a><span>'+data.hours+':'+data.minutes+'</span><p>'+data.message+'</p></div>')
 			} 
 		});
+
 		return (
             <div className="chat">
                 <div className="ui segment">
                     <div className="ui five column grid">
-                        <div className="column">
+                        <div className="">
                             <div className="ui segment">
                                 <div className="ui top attached label">
                                     <div className="ui two column grid">
@@ -117,15 +97,14 @@ class Chat extends Component{
                                     </div>
                                 </div>
                         	</div>
-                            <div className="ui fluid search selection dropdown" id="usersConnected">
-                                    <input type="hidden" name="country"/>
-                                    <i className="dropdown icon"></i>
-                                    <div className="default text">
-										Select User
-									</div>
-                                    <div className="menu" id="users">
-                                    </div>
-                            </div>
+
+							<Select
+								// value={selectedOption}
+								onChange={this.handleChangeUser}
+								options={this.state.userConnectedList}
+								placeholder="Select User"
+							/>
+							
                             <div className="ui segment" id="messages">
 
                             </div>
@@ -134,7 +113,7 @@ class Chat extends Component{
                                     <textarea rows="2"></textarea>
                                 </div>
                             </div>
-                            <button className="fluid ui right labeled icon button">
+                            <button className="fluid ui right labeled icon button" onClick={()=>{this.sendMessage()}}>
                                 <i className="right arrow icon"></i>
                                 Send
                             </button>
