b6236d1d5cc9b85219c4bf6664881069597d844d
Martin Thenot <martin.thenot@mastertp>
2019-10-25 12:38:11 +0200

    Added notification during ce game.

--- frontEnd/reactjs/src/play/components/game/containers/game.js
+++ frontEnd/reactjs/src/play/components/game/containers/game.js
@@ -4,6 +4,7 @@ import Chat from './../../../../chat/containers/chat'
 import User from './../../../../user/containers/User'
 import { connect } from 'react-redux';
 import { setMainMenuPage } from '../../../../actions';
+import NotificationAlert from 'react-notification-alert';
 const axios = require('axios').default;
 
 class Game extends Component{
@@ -25,6 +26,136 @@ class Game extends Component{
         this.sendEndTurn = this.sendEndTurn.bind(this);
         this.isMyTurnToPlay = this.isMyTurnToPlay.bind(this);
         this.setHome=this.setHome.bind(this);
+        this.updateInfoGame=this.updateInfoGame.bind(this);
+
+        var that = this;
+
+        // Mon adversaire m'attaque
+        this.state.socket.on('sendAttack', function(newMyCardSelectedHp){
+            // Si l'utilisateur est joueur 1, Sinon l'utilisateur est le joueur 2
+            if(that.props.user.id===that.state.player1.id){
+                console.log("=================================");
+                console.log("Le joueur 2 m'a attaque. Il me reste: "+newMyCardSelectedHp+" hp sur ma carte.");
+                that.updateInfoGame("danger","Aie vous avez recu une attaque!");
+                // On met a jour les valeurs de nos cartes
+                var newMyCardSelected = that.state.player1CardSelected;
+                newMyCardSelected.hp = newMyCardSelectedHp;
+                console.log(JSON.stringify(newMyCardSelected));
+                that.setState({
+                    player1CardSelected: newMyCardSelected,
+                })
+                console.log(JSON.stringify(that.state.player1CardSelected));
+
+            }else{
+                console.log("=================================");
+                console.log("Le joueur 1 m'a attaque. Il me reste: "+newMyCardSelectedHp+" hp sur ma carte.");
+                that.updateInfoGame("danger","Aie vous avez recu une attaque!");
+                // On met a jour les valeurs de nos cartes
+                newMyCardSelected = that.state.player2CardSelected;
+                newMyCardSelected.hp = newMyCardSelectedHp;
+                console.log(JSON.stringify(newMyCardSelected));
+                that.setState({
+                    player2CardSelected: newMyCardSelected,
+                })
+                console.log(JSON.stringify(that.state.player2CardSelected));
+
+            }
+        });
+
+        // Je recois la confirmation de mon attaque
+        this.state.socket.on('confirmedAttack', function(newMyCardSelectedHp){
+            // Si l'utilisateur est joueur 1, Sinon l'utilisateur est le joueur 2
+            if(that.props.user.id===that.state.player1.id){
+                console.log("Je suis le playeur 1. J'ai recu la confirmation de mon attaque.");
+                that.updateInfoGame("success","Vous avez envoye une attaque!");
+                // On met a jour les valeurs de la carte
+                var newOppositePlayerCardSelected = that.state.player1CardSelected;
+                newOppositePlayerCardSelected.hp = newMyCardSelectedHp;
+                that.setState({
+                    player2CardSelected: newOppositePlayerCardSelected,
+                })
+
+            }else{
+                console.log("Je suis le playeur 2. J'ai recu la confirmation de mon attaque.");
+                that.updateInfoGame("success","Vous avez envoye une attaque!");
+                // On met a jour les valeurs de la carte
+                newOppositePlayerCardSelected = that.state.player2CardSelected;
+                newOppositePlayerCardSelected.hp = newMyCardSelectedHp;
+                that.setState({
+                    player1CardSelected: newOppositePlayerCardSelected,
+                })
+            }
+        });
+
+        // Mon adversaire passe son tour
+        this.state.socket.on('sendEndTurn', function(){
+            // Si l'utilisateur est joueur 1, Sinon l'utilisateur est le joueur 2
+            if(that.props.user.id===that.state.player1.id){
+                console.log("Le joueur 2 a fini son tour.");
+                that.updateInfoGame("info","A vous de jouer!");
+                that.setState({
+                    currentPlayerIsPlayer1: true,
+                })
+            }else{
+                console.log("Le joueur 1 a fini son tour.");
+                that.updateInfoGame("info","A vous de jouer!");
+                that.setState({
+                    currentPlayerIsPlayer1: false,
+                })            
+            }		
+        });
+
+        this.state.socket.on('youLoose', function(){
+            console.log("J'ai perdu");
+            // TODO: - Popup looser
+
+            // Redirect to the home page
+            that.setHome();
+        })
+
+        this.state.socket.on('youWin', function(){
+            var those = that;
+            console.log("J'ai Gagne");
+            // TODO: - Popup avec firework
+
+            // Credit $1000 to the bank account via axios
+            // TODO: test
+            axios({
+                method: 'put',
+                baseURL: 'http://localhost:8082',
+                url:`/user/${that.props.user.id}`,
+                data:
+                {
+                    surname:that.props.user.surName,
+                    lastname:that.props.user.lastName,
+                    login:that.props.user.login,
+                    pwd:that.props.user.pwd,
+                    account:that.props.user.account+1000,
+                    img:that.props.user.img,
+
+                },
+                headers:{
+                    'Access-Control-Allow-Origin':'*'
+                }
+                })
+                .then(function(response){;
+                console.log("Credit the money RESPONSE: "+JSON.stringify(response));
+
+                console.log("Credit the money: "+JSON.stringify(response.data));
+                those.setHome();
+                those.setHome();
+
+                })
+                .catch(function(error){
+                    console.log("Credit the money error: "+error);
+                    // REDIRIGER TO LOGIN - MAYBE
+                });
+        
+            // Redirect to the home page
+            that.setHome();
+        })
+
+
     }
 
     handlePlayer1CardSelection(card){
@@ -37,6 +168,24 @@ class Game extends Component{
 
     setHome(){
         this.props.dispatch(setMainMenuPage());
+    }
+
+    updateInfoGame(infoType,messageSent){
+        const options = {
+            place: 'tc',
+            message: (
+                <div>
+                    <div>
+                        {messageSent}
+                    </div>
+                </div>
+            ),
+            type: infoType,
+            icon: "now-ui-icons ui-1_bell-53",
+            autoDismiss: 7
+        }
+        
+        this.refs.notify.notificationAlert(options);
     }
 
     isMyTurnToPlay(){
@@ -116,7 +265,6 @@ class Game extends Component{
         if(this.props.user.id===this.props.player1.id){
             // Si c'est au tour du joueur 1 de jouer
             if(this.state.currentPlayerIsPlayer1){
-                console.log("JE PASSE MON TOUR");
                 this.state.socket.emit('switchTurn', {
                     turnNext : this.state.player2,
                 });
@@ -125,12 +273,12 @@ class Game extends Component{
                 this.setState({
                     currentPlayerIsPlayer1: false,
                     numberOfAttacks: this.props.user.cardList.length
-                })
+                });
+                this.updateInfoGame("info","Vous avez passe votre tour.");
             }
         }else{
             // Si c'est au joueur 2 de jouer
             if(!this.state.currentPlayerIsPlayer1){
-                console.log("JE PASSE MON TOUR");
                 this.state.socket.emit('switchTurn', {
                     turnNext : this.state.player1,
                 });
@@ -139,126 +287,17 @@ class Game extends Component{
                 this.setState({
                     currentPlayerIsPlayer1: true,
                     numberOfAttacks: this.props.user.cardList.length
-                })
+                });
+                this.updateInfoGame("info","Vous avez passe votre tour.");
             }
         }
     }
 
     render() {
-        // console.log("This is my user in Game:"+JSON.stringify(this.props.user));
-        var that = this;
-
-        // Mon adversaire m'attaque
-        this.state.socket.on('sendAttack', function(newMyCardSelectedHp){
-            // Si l'utilisateur est joueur 1, Sinon l'utilisateur est le joueur 2
-            if(that.props.user.id===that.state.player1.id){
-                console.log("=================================");
-                console.log("Le joueur 2 m'a attaque. Il me reste: "+newMyCardSelectedHp+" hp sur ma carte.");
-                // On met a jour les valeurs de nos cartes
-                var newMyCardSelected = that.state.player1CardSelected;
-                newMyCardSelected.hp = newMyCardSelectedHp;
-                console.log(JSON.stringify(newMyCardSelected));
-                that.setState({
-                    player1CardSelected: newMyCardSelected,
-                })
-                console.log(JSON.stringify(that.state.player1CardSelected));
-
-            }else{
-                console.log("=================================");
-                console.log("Le joueur 1 m'a attaque. Il me reste: "+newMyCardSelectedHp+" hp sur ma carte.");
-                // On met a jour les valeurs de nos cartes
-                newMyCardSelected = that.state.player2CardSelected;
-                newMyCardSelected.hp = newMyCardSelectedHp;
-                console.log(JSON.stringify(newMyCardSelected));
-                that.setState({
-                    player2CardSelected: newMyCardSelected,
-                })
-                console.log(JSON.stringify(that.state.player2CardSelected));
-
-            }
-        });
-
-        // Je recois la confirmation de mon attaque
-        this.state.socket.on('confirmedAttack', function(newMyCardSelectedHp){
-            // Si l'utilisateur est joueur 1, Sinon l'utilisateur est le joueur 2
-            if(that.props.user.id===that.state.player1.id){
-                console.log("Je suis le playeur 1. J'ai recu la confirmation de mon attaque.");
-                // On met a jour les valeurs de la carte
-                var newOppositePlayerCardSelected = that.state.player1CardSelected;
-                newOppositePlayerCardSelected.hp = newMyCardSelectedHp;
-                that.setState({
-                    player2CardSelected: newOppositePlayerCardSelected,
-                })
-
-            }else{
-                console.log("Je suis le playeur 2. J'ai recu la confirmation de mon attaque.");
-                // On met a jour les valeurs de la carte
-                newOppositePlayerCardSelected = that.state.player2CardSelected;
-                newOppositePlayerCardSelected.hp = newMyCardSelectedHp;
-                that.setState({
-                    player1CardSelected: newOppositePlayerCardSelected,
-                })
-            }
-        });
-
-        // Mon adversaire passe son tour
-        this.state.socket.on('sendEndTurn', function(){
-            // Si l'utilisateur est joueur 1, Sinon l'utilisateur est le joueur 2
-            if(that.props.user.id===that.state.player1.id){
-                console.log("Le joueur 2 a fini son tour.");
-                that.setState({
-                    currentPlayerIsPlayer1: true,
-                })
-            }else{
-                console.log("Le joueur 1 a fini son tour.");
-                that.setState({
-                    currentPlayerIsPlayer1: false,
-                })            
-            }		
-        });
-
-        this.state.socket.on('youLoose', function(){
-            console.log("J'ai perdu");
-            // TODO: - Popup looser
-
-            // Redirect to the home page
-            this.setHome();
-        })
-
-        this.state.socket.on('youWin', function(){
-            console.log("J'ai Gagne");
-            // TODO: - Popup avec firework
-
-            // Credit $1000 to the bank account via axios
-            // TODO: test
-            axios({
-                method: 'put',
-                baseURL: 'http://localhost:8082',
-                url:`/user/${this.props.user.id}`,
-                data:
-                {
-                    account:this.props.user.account+1000,
-                },
-                headers:{
-                    'Access-Control-Allow-Origin':'*'
-                }
-              })
-              .then(function(response){;
-                  
-                console.log("Credit the money: "+JSON.stringify(response));
-          
-              })
-              .catch(function(error){
-                  console.log("Credit the money error: "+error);
-                  // REDIRIGER TO LOGIN - MAYBE
-              });
-        
-            // Redirect to the home page
-            this.setHome();
-        })
 
         return (
                 <div className="ui fluid">
+                    <NotificationAlert ref="notify" />
                     <div className="ui grid">
                         <div className="four wide column">
                             <Chat user={this.props.user}></Chat>
